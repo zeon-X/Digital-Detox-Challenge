7c6f94f2694e60c3b3e101638741371758e2bcaa
Md. Shefat Zeon <mdshefatzeon@gmail.com>
2024-11-14 03:38:19 +0600

    drawer added

--- app/src/main/java/com/aleehatech/digitaldetoxchallange/MainActivity.kt
+++ app/src/main/java/com/aleehatech/digitaldetoxchallange/MainActivity.kt
@@ -1,25 +1,79 @@
 package com.aleehatech.digitaldetoxchallange
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.aleehatech.digitaldetoxchallange.databinding.ActivityMainBinding
 import com.aleehatech.digitaldetoxchallange.utils.Utils.promptUserToEnableAccessibilityService
 import com.aleehatech.digitaldetoxchallange.utils.Utils.requestUsageAccess
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+
+
+
 
         // Prompt user to enable the Focus Mode Accessibility Service
         promptUserToEnableAccessibilityService(this, FocusModeAccessibilityService::class.java)
         requestUsageAccess(this)
+
+
+
+
+
     }
 
     override fun onResume() {
         super.onResume()
         promptUserToEnableAccessibilityService(this, FocusModeAccessibilityService::class.java)
         requestUsageAccess(this)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
